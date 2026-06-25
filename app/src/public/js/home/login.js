001e805535a2027355a96bbc611b0b7b32f97cbc
이근희 <a87627648a@gmail.com>
2026-06-26 03:20:15 +0900

    로그인을 요청하는 fetch 생성

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -10,7 +10,17 @@ function login() {
     const req={
         id: id.value,
         psword: psword.value,
-    }
+    };
+
+  
+    fetch("/login", {
+        method: "POST",
+        headers:{
+            "Content-Type":application/json,
+        },
+        body: JSON.stringify(req),
+    });
+
 }
 
 
